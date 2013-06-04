7a21b0bb23cfe2f8c5bc70cce24bbecc42e57975
simonforeveryoung <simonforeveryoung@gmail.com>
2013-06-04 13:27:43 -0700

    Initial commit

---
+++ MyFirstProjectInworkspace/MyFirstTargetInAProject/NIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NIAppDelegate.h
+//  MyFirstTargetInAProject
+//
+//  Created by SIMON YOUNG on 6/3/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyFirstProjectInworkspace/MyFirstTargetInAProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyFirstTargetInAProject
+//
+//  Created by SIMON YOUNG on 6/3/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NIAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NIAppDelegate class]));
+    }
+}
